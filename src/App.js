2bb663138b6500c71c92e153ff32f5baff44fb22
unknown <ramdurga.giet@gmail.com>
2019-08-26 21:05:55 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Go-aLs</h2>
+        </div>
+        <p className="App-intro">
+          Set your goals here. We take job to remind you.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
